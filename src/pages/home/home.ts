2a18eb6a55e595351059b12c6293cb3539b3c89d
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2018-01-09 17:08:33 +0200

    display articles

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 import {ServiceClientProvider} from "../../providers/service-client/service-client";
+import {TopicsProvider} from "../../providers/topics/topics";
 
 
 @Component({
@@ -9,12 +10,20 @@ import {ServiceClientProvider} from "../../providers/service-client/service-clie
 })
 export class HomePage {
 
+  public articles: Array<any>;
+
   constructor(private navCtrl: NavController,
-              private newSumService: ServiceClientProvider
-  ) {
-    this.newSumService = newSumService;
+              private topicsProvider: TopicsProvider) {
   }
 
+  ionViewDidLoad() {
+    this.articles = this.topicsProvider.getTopics();
+    console.log(this.articles);
+    this.topicsProvider.topicsUpdated.subscribe((newTopics) => {
+      if(newTopics)
+        this.articles = newTopics;
+    }, error2 => console.log(error2));
+  }
 
 
 }
