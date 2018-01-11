9a45a85035d763543e28079f3a449c393d3b6f33
Spyros Nikolakis <nikolakispyros@gmail.com>
2018-01-11 18:04:27 +0200

    default category images should be displayed on android phones

--- src/lib/categories-view-manager.ts
+++ src/lib/categories-view-manager.ts
@@ -1,14 +1,14 @@
 export class CategoriesViewManager {
   private static map = {
-    'Επιστήμη': '../../assets/imgs/categories/science.jpg',
-    'Οικονομία': '../../assets/imgs/categories/economics.jpg',
-    'Κόσμος': '../../assets/imgs/categories/world3.jpg',
-    'Πολιτισμός': '../../assets/imgs/categories/civilization.jpg',
-    'Αθλητισμός': '../../assets/imgs/categories/sports.jpg',
-    'Τεχνολογία': '../../assets/imgs/categories/tech.jpg',
-    'Ελλάδα': '../../assets/imgs/categories/civilization2.jpg',
-    'SciFY News': '../../assets/imgs/categories/scify_logo.png',
-    'Γενικά': '../../assets/imgs/categories/newspaper.jpg'
+    'Επιστήμη': 'assets/imgs/categories/science.jpg',
+    'Οικονομία': 'assets/imgs/categories/economics.jpg',
+    'Κόσμος': 'assets/imgs/categories/world3.jpg',
+    'Πολιτισμός': 'assets/imgs/categories/civilization.jpg',
+    'Αθλητισμός': 'assets/imgs/categories/sports.jpg',
+    'Τεχνολογία': 'assets/imgs/categories/tech.jpg',
+    'Ελλάδα': 'assets/imgs/categories/civilization2.jpg',
+    'SciFY News': 'assets/imgs/categories/scify_logo.png',
+    'Γενικά': 'assets/imgs/categories/newspaper.jpg'
   };
 
   public static getCategoryDefaultImage(category: string): string {
