5d74c2143ffcff325ffac16c618915204f404061
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2018-01-17 14:22:33 +0200

    ability to show hide sources on summary page

--- src/pages/summary/summary.ts
+++ src/pages/summary/summary.ts
@@ -1,10 +1,12 @@
 import {Component, ViewChild} from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController} from 'ionic-angular';
 import {TopicsProvider} from "../../providers/topics/topics";
 import {SummariesProvider} from "../../providers/summaries/summaries";
 import {CategoriesProvider} from "../../providers/categories/categories";
 import {CategoriesViewManager} from "../../lib/categories-view-manager";
 import {GoogleAnalytics} from '@ionic-native/google-analytics';
+import {Storage} from "@ionic/storage";
+
 
 /**
  * Generated class for the SummaryPage page.
@@ -23,17 +25,30 @@ export class SummaryPage {
   public selectedCategoryDefaultImage: string;
   public selectedTopic: any;
   public selectedSummary: any;
+  public displaySourcesUponEachSentence: boolean = true;
+  public summaryIsConstructedByMoreThanOneSources: boolean;
 
   constructor(private navCtrl: NavController, private topicsProvider: TopicsProvider,
-              private summaryProvider: SummariesProvider, private categoriesProvider: CategoriesProvider,
-              protected ga: GoogleAnalytics) {
+              private summaryProvider: SummariesProvider,
+              private categoriesProvider: CategoriesProvider,
+              private ga: GoogleAnalytics,
+              private storage: Storage) {
     this.selectedSummary = this.summaryProvider.getSummary();
     if (this.selectedSummary) {
       this.selectedCategory = this.categoriesProvider.getSelectedCategory();
       this.selectedCategoryDefaultImage = CategoriesViewManager.getCategoryDefaultImage(this.selectedCategory);
       this.selectedTopic = this.topicsProvider.getSelectedTopic();
+      this.summaryIsConstructedByMoreThanOneSources = this.selectedSummary.Sources.length > 1;
       this.ga.trackView("Summary: " + this.selectedTopic.Title);
-      console.log(this.selectedSummary);
     }
+    storage.ready().then(() => {
+      this.storage.get("displaySources").then((val) => {
+        this.displaySourcesUponEachSentence = val == 1;
+      })
+    });
+  }
+
+  public toggleDisplaySources() {
+    this.storage.set("displaySources", this.displaySourcesUponEachSentence);
   }
 }
