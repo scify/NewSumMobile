a4874604c52e1ff4daf0122bfbd854847e821bff
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2018-01-19 17:52:58 +0200

    remove delay on api calls

--- src/providers/soap-client/soap-client.ts
+++ src/providers/soap-client/soap-client.ts
@@ -19,7 +19,7 @@ export class SoapClientProvider {
     return new Promise((resolve, reject) => {
       const results = JSON.parse(this.soapClient.invoke(APP_CONFIG.apiEndpoint + APP_CONFIG.wsdlPath, methodToInvoke, parameters, false,null));
 
-      window.setTimeout(()=>resolve(results),2000);
+      resolve(results);
 
     });
   }
