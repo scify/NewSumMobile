bfb1e174d3d42d6b62e57eff94ba392f22420b7b
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2018-01-31 15:30:15 +0200

    Add export to translation module config

--- src/app/translation-module-config.ts
+++ src/app/translation-module-config.ts
@@ -2,7 +2,7 @@ import {TranslateLoader} from "@ngx-translate/core";
 import {HttpClient} from "@angular/common/http";
 import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 
-function createTranslationLoader(http: HttpClient) {
+export function createTranslationLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
